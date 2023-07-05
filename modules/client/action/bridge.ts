100a168e98ff50802d3b4522ae2134a3e2670a37
Julio Rodriguez <jircdev@gmail.com>
2023-07-04 23:06:01 -0300

    fix: Handle disconnect/error events on bridges, resolves 'Unable to hear disconnect/error event on bridges when breaking connection' (#1)
    This commit addresses the following issue:
    - Fixed the issue where disconnect and error events were not being detected on bridges when the connection was broken
    
    Closes #1

--- modules/client/action/bridge.ts
+++ modules/client/action/bridge.ts
@@ -1,20 +1,25 @@
-import Action from "./";
-import {Bundle} from '@beyond-js/kernel/bundle';
-
+import Action from './';
+import { Bundle } from '@beyond-js/kernel/bundle';
+import { backends } from '../backends';
 export /*bundle*/
 class ActionsBridge {
-    readonly #distribution: string;
-    readonly #bundle: string;
-    readonly #backend: string;
+	readonly #distribution: string;
+	readonly #bundle: string;
+	#pkg: string;
+	readonly #backend: string;
 
-    constructor(distribution: string, bundle: Bundle) {
-        this.#distribution = distribution;
-        this.#bundle = bundle.specifier;
-        this.#backend = `${bundle.module.pkg}/${this.#distribution}`;
-    }
+	get backend() {
+		return backends.get(this.#pkg);
+	}
+	constructor(distribution: string, bundle: Bundle) {
+		this.#distribution = distribution;
+		this.#bundle = bundle.specifier;
+		this.#pkg = bundle.module.pkg;
+		this.#backend = `${bundle.module.pkg}/${this.#distribution}`;
+	}
 
-    async execute(action: string, ...params: any[]): Promise<any> {
-        const a = new Action(this.#backend, this.#bundle, action, ...params);
-        return await a.execute();
-    }
+	async execute(action: string, ...params: any[]): Promise<any> {
+		const a = new Action(this.#backend, this.#bundle, action, ...params);
+		return await a.execute();
+	}
 }
