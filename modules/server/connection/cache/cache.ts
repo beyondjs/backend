46eabadfd29dfc5ee77bed7cb39fd9c833b66704
=Julio Rodriguez <jircdev@gmail.com>
2023-07-07 17:05:08 -0300

    feat: Add WebSocket response caching and connection limit configuration
    This commit adds two new configuration constants,  and , to the .env file.
    
     allows setting the duration for caching response answers, improving performance by avoiding redundant computations within the specified timeframe.
    
     sets the maximum number of concurrent WebSocket connections that the library can handle, preventing overload and ensuring resource allocation efficiency.
    
    These additions enhance the library's flexibility and adaptability, optimizing its functionality for different deployment scenarios.
    
    Affected files:
    - .env
    
    Please review and merge these changes into the main branch.

--- modules/server/connection/cache/cache.ts
+++ modules/server/connection/cache/cache.ts
@@ -1,53 +1,57 @@
+const EXPIRATION = (() => {
+	if (!process.env.CACHE_EXPIRATION_TIME || isNaN(parseInt(process.env.CACHE_EXPIRATION_TIME))) return 60000;
+	return parseInt(process.env.CACHE_EXPIRATION_TIME);
+})();
 export class Cache {
-    #socketId: string;
-    #cache: Map<string, any> = new Map();
-    #times: { key: string, time: number }[] = [];
-    #EXPIRATION = 30000; // 30 sec of expiration time
+	#socketId: string;
+	#cache: Map<string, any> = new Map();
+	#times: { key: string; time: number }[] = [];
+	#EXPIRATION = EXPIRATION; // 30 sec of expiration time
 
-    constructor(socketId: string) {
-        this.#socketId = socketId;
-        setInterval(this.#clean, 1000);
-    }
+	constructor(socketId: string) {
+		this.#socketId = socketId;
+		setInterval(this.#clean, 1000);
+	}
 
-    #clean = () => {
-        if (!this.#times.length) return;
+	#clean = () => {
+		if (!this.#times.length) return;
 
-        const expired = Date.now() - this.#EXPIRATION;
-        let rq = this.#times[0];
-        if (rq.time > expired) return;
+		const expired = Date.now() - this.#EXPIRATION;
+		let rq = this.#times[0];
+		if (rq.time > expired) return;
 
-        // Expire item
-        this.#times.shift();
-        this.#cache.delete(rq.key);
-    }
+		// Expire item
+		this.#times.shift();
+		this.#cache.delete(rq.key);
+	};
 
-    has(key: string): boolean {
-        key = `${this.#socketId}.${key}`
-        return this.#cache.has(key);
-    }
+	has(key: string): boolean {
+		key = `${this.#socketId}.${key}`;
+		return this.#cache.has(key);
+	}
 
-    get(key: string): any {
-        key = `${this.#socketId}.${key}`
-        return this.#cache.get(key);
-    }
+	get(key: string): any {
+		key = `${this.#socketId}.${key}`;
+		return this.#cache.get(key);
+	}
 
-    insert(key: string, value: any) {
-        key = `${this.#socketId}.${key}`;
-        if (this.#cache.has(key)) {
-            console.error(`Cache key "${key}" already set`);
-            this.update(key, value);
-            return;
-        }
+	insert(key: string, value: any) {
+		key = `${this.#socketId}.${key}`;
+		if (this.#cache.has(key)) {
+			console.error(`Cache key "${key}" already set`);
+			this.update(key, value);
+			return;
+		}
 
-        this.#cache.set(key, value);
-        this.#times.push({'key': key, 'time': Date.now()});
-    }
+		this.#cache.set(key, value);
+		this.#times.push({ key: key, time: Date.now() });
+	}
 
-    update(key: string, value: any) {
-        key = `${this.#socketId}.${key}`;
-        if (!this.#cache.has(key)) {
-            console.warn(`Cache key "${key}" not set`);
-        }
-        this.#cache.set(key, value);
-    }
+	update(key: string, value: any) {
+		key = `${this.#socketId}.${key}`;
+		if (!this.#cache.has(key)) {
+			console.warn(`Cache key "${key}" not set`);
+		}
+		this.#cache.set(key, value);
+	}
 }
